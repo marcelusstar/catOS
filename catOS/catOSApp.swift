696a9b7851ed61d6fc4ff3a58b9d25ca119eef1d
starmarcelo <29465737+starmarcelo@users.noreply.github.com>
2023-11-19 11:33:12 +0100

    Created empty xcode project

---
+++ catOS/catOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  catOSApp.swift
+//  catOS
+//
+//  Created by ftbtappx on 19/11/23.
+//
+
+import SwiftUI
+
+@main
+struct catOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
