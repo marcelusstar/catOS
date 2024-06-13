d87ee6bc12c17614fd35518f39256c76629a0ff2
marcelusstar <29465737+marcelusstar@users.noreply.github.com>
2024-06-13 12:40:47 +0200

    Remove unused code

--- catOS/catOSApp.swift
+++ catOS/catOSApp.swift
@@ -11,29 +11,6 @@ struct catOSApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .onAppear {
-                   // Call the doAsyncAwaitRequest function asynchronously
-                   Task {
-                           /*
-                           let voteResult: VoteResultEntity = try await RequestManager.shared.doAsyncAwaitRequest(apiRouter: ApiRouter.voteImage("an7", "soyelctangana", 1))
-                            */
-                           /*
-                           let breeds: [Breed] = await ApiManager.shared.getBreeds()
-                           for breed in breeds {
-                               print(breed.name)
-                           }*/
-                                             
-                       do {
-                           let feedImages: [FeedImage] = try await ApiManager.shared.getFeedImages(page: 1)
-                           for feedImage in feedImages {
-                               print(feedImage.id)
-                           }
-                       } 
-                       catch {
-                           ErrorManager.shared.process(error as! CatError)
-                       }
-                   }
-               }
         }
     }
 }
