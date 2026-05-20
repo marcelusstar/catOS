a0596765d935a3d3df63959032a00c348e5a8a2b
marcelusstar <29465737+marcelusstar@users.noreply.github.com>
2026-05-20 13:28:15 +0200

    refactor: removing unused methods from navigator

--- catOS/UI/Coordinator/Navigator.swift
+++ catOS/UI/Coordinator/Navigator.swift
@@ -10,8 +10,6 @@ import SwiftUI
 class Navigator: ObservableObject {
     
     @Published var path = NavigationPath()
-    @Published var sheet: CatScreens?
-    @Published var fullScreenCover: CatScreens?
     @Published var selectedTab: Tab = .feed
     
     func push(_ navigationView: CatScreens) {
@@ -24,21 +22,5 @@ class Navigator: ObservableObject {
     
     func popToRoot() {
         path.removeLast(path.count)
-    }
-    
-    func presentSheet(_ navigationView: CatScreens) {
-        self.sheet = navigationView
-    }
-    
-    func dismissSheet() {
-        self.sheet = nil
-    }    
-    
-    func presentFullScreen(_ navigationView: CatScreens) {
-        self.fullScreenCover = navigationView
-    }
-    
-    func dismissFullScreen() {
-        self.fullScreenCover = nil
     }
 }
